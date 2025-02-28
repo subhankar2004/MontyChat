41035056a02188018d7e7b21d46b5c2cf20b85b1
Subhankar Patra <patrasubhankar2205@gmail.com>
2025-02-28 21:42:35 +0530

    enabled cors

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,13 @@ const __dirname =path.resolve();
 const server = http.createServer(app);
 
 
-// const corsOptions = {
-//   origin: "http://localhost:5173",
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// };
-//app.use(cors(corsOptions));
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
