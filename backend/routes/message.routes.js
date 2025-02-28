cc9251f9b18d53ac01d3bf87deb36cf5d10ebd5d
Subhankar Patra <patrasubhankar2205@gmail.com>
2025-02-28 21:11:33 +0530

    path fixed

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import {sendMessage,getMessages} from '../controllers/message.controller.js';
 import protectRoute from '../middleware/protectroute.js';
-
 const router=express.Router();
 
 router.get('/:userId',protectRoute,getMessages);
